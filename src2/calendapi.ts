5a30a79f7b5cdbac7b841dfa843f2ed6018fc5a8
Kirill Tikhonov <lliriq@me.com>
2022-02-26 23:34:04 +0300

    fix

--- src2/calendapi.ts
+++ src2/calendapi.ts
@@ -1,81 +1,66 @@
 import { Storage } from "./calendobj";
 
 export interface StorageServ {
-
-    create: (x: Storage) => Promise<void>,
-    read: () => Promise<Storage[] | []>,
-    update: (x: Storage) => Promise<void>,
-    delete: (x: Storage) => Promise<void>,
-    filterDate: (x: number) => Promise<Storage[]>,
-    filterDescript: (x: string) => Promise<Storage[]>,
-    filterStatus: (x: string) => Promise<Storage[]>,
-    filterTag: (x: string) => Promise<Storage[]>
-
+  create: (x: Storage) => Promise<void>;
+  read: () => Promise<Storage[] | []>;
+  update: (x: Storage) => Promise<void>;
+  delete: (x: Storage) => Promise<void>;
+  filterDate: (x: number) => Promise<Storage[]>;
+  filterDescript: (x: string) => Promise<Storage[]>;
+  filterStatus: (x: string) => Promise<Storage[]>;
+  filterTag: (x: string) => Promise<Storage[]>;
 }
 
 export class Service implements StorageServ {
+  async create(item: Storage) {
+    const store = await this.read();
+    store.push(item);
+    localStorage.setItem("store", JSON.stringify(store));
+  }
 
-    async create(item: Storage) {
+  read = async () => {
+    const store: string | null = localStorage.getItem("store");
+    if (store) return JSON.parse(store);
+    return [];
+  };
 
-        const store = await this.read();
-        store.push(item);
-        localStorage.setItem('store', JSON.stringify(store));
+  async update(item: Storage) {
+    const store = await this.read();
+
+    for (let i = 0; i < store.length; i += 1) {
+      if (item.id === store[i].id) store[i] = item;
     }
 
-    read = async () => {
+    localStorage.setItem("store", JSON.stringify(store));
+  }
 
-        const store: string | null = localStorage.getItem('store');
-        if (store) return JSON.parse(store);
-        return [];
+  async delete(item: Storage) {
+    const store = await this.read();
+
+    for (let i = 0; i < store.length; i += 1) {
+      if (item.id === store[i].id) store.splice(i, 1);
     }
 
-    async update(item: Storage) {
+    localStorage.setItem("store", JSON.stringify(store));
+  }
 
-        const store = await this.read();
+  async filterDate(someDate: number) {
+    const store: Storage[] = await this.read();
+    return store.filter((item) => item.date === someDate);
+  }
 
-        for (let i = 0; i < store.length; i += 1) {
+  async filterDescript(dscrpt: string) {
+    const store: Storage[] = await this.read();
+    return store.filter((item) => item.description.includes(dscrpt));
+  }
 
-            if (item.id === store[i].id) store[i] = item;
-        }
+  async filterStatus(stat: string) {
+    const store: Storage[] = await this.read();
+    return store.filter((item) => item.status === stat);
+  }
 
-        localStorage.setItem('store', JSON.stringify(store));
-    }
-
-    async delete(item: Storage) {
-
-        const store = await this.read();
-
-        for (let i = 0; i < store.length; i += 1) {
-
-            if (item.id === store[i].id) store.splice(i, 1);
-        }
-
-        localStorage.setItem('store', JSON.stringify(store));
-    }
-
-    async filterDate(someDate: number) {
-
-        const store: Storage[] = await this.read();
-        return store.filter(item => item.date === someDate);
-    }
-
-    async filterDescript(dscrpt: string) {
-
-        const store: Storage[] = await this.read();
-        return store.filter(item => item.description.includes(dscrpt));
-    }
-
-    async filterStatus(stat: string) {
-
-        const store: Storage[] = await this.read();
-        return store.filter(item => item.status === stat);
-
-    }
-
-    async filterTag(tag: string) {
-
-        const store: Storage[] = await this.read();
-        return store.filter(item => item.tag === tag);
-    }
-
+  async filterTag(tag: string) {
+    const store: Storage[] = await this.read();
+    return store.filter((item) => item.tag === tag);
+  }
 }
